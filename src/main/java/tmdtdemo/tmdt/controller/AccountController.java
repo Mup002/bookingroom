a81d43b20bb4818c8a2e02fd76544bbf36ace330
Mup002 <narutovvv1@gmail.com>
2024-03-07 14:57:00 +0700

    validate

--- src/main/java/tmdtdemo/tmdt/controller/AccountController.java
+++ src/main/java/tmdtdemo/tmdt/controller/AccountController.java
@@ -1,11 +1,15 @@
 package tmdtdemo.tmdt.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import tmdtdemo.tmdt.dto.request.UserRequest;
 import tmdtdemo.tmdt.service.UserService;
-import tmdtdemo.tmdt.utils.BaseResponse;
+import tmdtdemo.tmdt.utils.ValidateObject;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -14,7 +18,11 @@ import tmdtdemo.tmdt.utils.BaseResponse;
 public class AccountController {
     private final UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<BaseResponse> register(@RequestBody UserRequest dto){
+    public ResponseEntity<Object> register(@RequestBody UserRequest dto){
+        Map<String, String> errorsValidate = ValidateObject.validateRegisterRequest(dto);
+        if(!ObjectUtils.isEmpty(errorsValidate)){
+            return new ResponseEntity<>(errorsValidate,HttpStatus.BAD_REQUEST);
+        }
         return ResponseEntity.ok(userService.register(dto));
     }
 }
---
+++ src/main/java/tmdtdemo/tmdt/utils/ValidateObject.java
@@ -0,0 +1,53 @@
+package tmdtdemo.tmdt.utils;
+
+import tmdtdemo.tmdt.dto.request.UserRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateObject {
+    public static Map<String, String> validateRegisterRequest(UserRequest dto){
+        Map<String, String> errors = new HashMap<>();
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("username")
+                .value(dto.getUsername())
+                .required(true)
+                .build().validate());
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("password")
+                .value(dto.getPassword())
+                .required(true)
+                        .min(Long.valueOf(1))
+                        .max(Long.valueOf(20))
+                .build().validate());
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("email")
+                .value(dto.getEmail())
+                .required(true)
+                .isValidEmail(true)
+                .build().validate());
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("gender")
+                .value(dto.getGender())
+                .required(true)
+                .build().validate());
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("date")
+                .value(dto.getGender())
+                .required(true)
+                .build().validate());
+
+        errors.putAll(ValidateUtils.builder()
+                .fieldName("role")
+                .value(dto.getGender())
+                .required(true)
+                .build().validate());
+
+        return errors;
+    }
+}
